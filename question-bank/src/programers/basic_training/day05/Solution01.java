56d872ee4452ef24ed33a740b2d82e45db6bb578
Jinpyo An <dkswlsvy3312@gmail.com>
2025-03-10 13:40:15 +0900

    programmers basic day05

---
+++ question-bank/src/programers/basic_training/day05/Solution01.java
@@ -0,0 +1,25 @@
+package programers.basic_training.day05;
+
+public class Solution01 {
+
+    public String solution(String code) {
+        StringBuilder ret = new StringBuilder();
+        int mode = 0;
+
+
+        for (int i = 0; i < code.length() ; i++) {
+            char current = code.charAt(i);
+
+            if (current == '1') {
+                mode = (mode == 0) ? 1:0;
+                continue;
+            }
+
+            if (i % 2 == mode) {
+                ret.append(current);
+            }
+        }
+
+        return ret.length() == 0 ? "EMPTY": ret.toString();
+    }
+}
