bd9f705b687b286b7284433a7bae1cce9b4cc00b
Jinpyo An <dkswlsvy3312@gmail.com>
2025-03-13 10:57:31 +0900

    programmers basic day07

--- question-bank/src/programers/basic_training/day06/Solution05.java
+++ question-bank/src/programers/basic_training/day06/Solution05.java
@@ -1,34 +1,39 @@
+/**
+ * 입력 값: 정수 배열 arr, 2차원 정수 배열 queries
+ * 1. queries 각 원소 query -> [s, e, k]
+ * 2. s <= i <= e인 모든 i에 대해 k보다 크면서 가장 작은 값 arr[i] 찾기
+ * 3. 쿼리가 존재하지 않으면 -1 저장
+ * 4. 각 쿼리 순서에 맞게 저장한 배열 반환
+
+ * 1. 쿼리를 저장할 리스트 answer
+ * 2. queries 순회
+ * 3. s, e, k 선언 및 초기화
+ * 4. for 문으로 가장 작은 값 i 찾기
+ * 5.
+ */
+
 package programers.basic_training.day06;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class Solution05 {
 
-//    다시 풀기
-//    public int[] solution(int[] arr, int[][] queries) {
-//        List<Integer> answer = new ArrayList<>();
-//
-//        for (int[] query : queries) {
-//            int s = query[0];
-//            int e = query[1];
-//            int k = query[2];
-//            int i = s;
-//            boolean check = false;
-//
-//            while (i <= e) {
-//                if (arr[i] > k) {
-//                    check = true;
-//                    break;
-//                }
-//                i++;
-//            }
-//
-//            if (check) {
-//                answer.add(arr[i]);
-//            } else answer.add(-1);
-//        }
-//
-//        return answer.stream().mapToInt(Integer::intValue).toArray();
-//    }
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = new int[queries.length];
+        Arrays.fill(answer, -1);
+
+        for (int idx = 0; idx < queries.length; idx++) {
+            int[] query = queries[idx];
+            int s = query[0], e = query[1], k = query[2];
+
+            for (int i = s; i <= e; i++) {
+                if (arr[i] > k) {
+                    answer[idx] = (answer[idx] == -1 ? arr[i] : Math.min(answer[idx], arr[i]));
+                }
+            }
+
+        }
+
+        return answer;
+    }
 }
---
+++ question-bank/src/programers/basic_training/day07/Solution04.java
@@ -0,0 +1,34 @@
+/**
+ * 입력값: 자연수 n
+ * <p>
+ * 1. 콜라츠 수열을 기록할 리스트 선언
+ * 2. n이 1이 될 때까지 다음 작업을 반복
+ * - n이 짝수일 때, 2로 나눈다.
+ * - n이 홀수일 때, 3 * n + 1
+ * 3. 결과 값을 리스트로 변환 후 반환
+ */
+
+package programers.basic_training.day07;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution04 {
+
+    public int[] solution(int n) {
+        List<Integer> answer = new ArrayList<>();
+        answer.add(n);
+
+        while (n != 1) {
+            if (n % 2 == 0) {
+                n /= 2;
+                answer.add(n);
+            } else {
+                n = (3 * n) + 1;
+                answer.add(n);
+            }
+        }
+
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
