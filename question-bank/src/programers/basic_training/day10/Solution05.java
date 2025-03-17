16b6d6be839b9d182eaa3fc9835b46cd6b32433d
Jinpyo An <dkswlsvy3312@gmail.com>
2025-03-17 15:53:39 +0900

    programmers basic day10

---
+++ question-bank/src/programers/basic_training/day10/Solution05.java
@@ -0,0 +1,24 @@
+/**
+ * 문자열 code, 두 정수 q, r
+ * code의 각 인덱스를 q로 나누었을 때, 나머지가 r인 위치의 문자를 순서대로 이어붙인 문자열 return
+ *
+ * 문자를 이어붙일 builder 변수
+ * 
+ */
+
+package programers.basic_training.day10;
+
+public class Solution05 {
+    
+    public String solution(int q, int r, String code) {
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < code.length(); i++) {
+            if (r == (i % q)) {
+                answer.append(code.charAt(i));
+            }
+        }
+
+        return answer.toString();
+    }
+}
