f93932700897f8b637dfe5420001e6d0122aa3b3
Jinpyo An <dkswlsvy3312@gmail.com>
2026-01-12 14:31:42 +0900

    3. 문장 속 단어

---
+++ online-lecture/src/main/java/chapter01/Solution3_1.java
@@ -0,0 +1,29 @@
+package chapter01;
+
+import java.util.Scanner;
+
+public class Solution3_1 {
+
+    public String solution(String str) {
+        String answer = "";
+        int m = Integer.MIN_VALUE;
+        String[] s = str.split(" ");
+
+        for (String x : s) {
+            int len = x.length();
+            if (len > m) {
+                m = len;
+                answer = x;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution3_1 T = new Solution3_1();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.nextLine();
+        System.out.println(T.solution(str));
+    }
+}
