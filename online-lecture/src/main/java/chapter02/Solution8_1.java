9de2aae02228adebbadf9c22652e99c6d320cc37
Jinpyo An <dkswlsvy3312@gmail.com>
2026-01-22 11:02:23 +0900

    8. 등수 구하기

---
+++ online-lecture/src/main/java/chapter02/Solution8_1.java
@@ -0,0 +1,32 @@
+package chapter02;
+
+import java.util.Scanner;
+
+public class Solution8_1 {
+
+    public static int[] solution(int n, int[] arr) {
+        int[] answer = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int cnt = 1;
+            for (int j = 0; j < n; j++) {
+                if (arr[i] < arr[j]) cnt++;
+            }
+            answer[i] = cnt;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        for (int i : solution(n, arr)) {
+            System.out.print(i + " ");
+        }
+    }
+}
