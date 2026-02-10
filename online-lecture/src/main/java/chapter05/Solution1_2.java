cc79066fa41bed229e273e72e787e32bd57cd8c9
Jinpyo An <dkswlsvy3312@gmail.com>
2026-02-10 11:22:09 +0900

    2. 괄호 문자 제거

---
+++ online-lecture/src/main/java/chapter05/Solution1_2.java
@@ -0,0 +1,33 @@
+package chapter05;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Solution1_2 {
+
+    public static String solution(String str) {
+        Deque<Character> deque = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
+
+        for(char ch : str.toCharArray()) {
+            if(ch == ')') {
+                while(true) {
+                    char result = deque.pop();
+                    if(result == '(') break;
+                }
+            } else deque.push(ch);
+        }
+
+        for(char ch: deque) sb.append(ch);
+
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        System.out.println(solution(str));
+    }
+}
