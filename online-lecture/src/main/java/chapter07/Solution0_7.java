42e7d36595569ff9753458a4194a0f2f83e1e9b5
Jinpyo An <dkswlsvy3312@gmail.com>
2026-03-03 11:05:53 +0900

    0. 부분집합 구하기(DFS)

---
+++ online-lecture/src/main/java/chapter07/Solution0_7.java
@@ -0,0 +1,29 @@
+package chapter07;
+
+public class Solution0_7 {
+
+    static int n;
+    static int[] ch;
+
+    public static void DFS(int L) {
+        if (L == n + 1) {
+            String tmp = "";
+            for (int i = 1; i <= n; i++) {
+                if (ch[i] == 1) tmp += (i + " ");
+            }
+            if (!tmp.isEmpty()) System.out.println(tmp);
+        }
+        else {
+            ch[L] = 1;
+            DFS(L + 1);
+            ch[L] = 0;
+            DFS(L + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        n = 3;
+        ch = new int[n + 1];
+        DFS(1);
+    }
+}
