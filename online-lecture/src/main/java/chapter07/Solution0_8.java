ca021f5ab406546d23a0751021a36a31fd3f970d
Jinpyo An <dkswlsvy3312@gmail.com>
2026-03-03 11:29:07 +0900

    0. 이진트리 레벨 탐색(BFS)

---
+++ online-lecture/src/main/java/chapter07/Solution0_8.java
@@ -0,0 +1,40 @@
+package chapter07;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Solution0_8 {
+
+    Node root;
+
+    public void BFS(Node root) {
+        Deque<Node> queue = new ArrayDeque<>();
+        queue.offer(root);
+        int L = 0;
+
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+            System.out.print(L + " : ");
+            for (int i = 0; i < len; i++) {
+                Node cur = queue.poll();
+                System.out.print(cur.data + " ");
+                if (cur.lt != null) queue.offer(cur.lt);
+                if (cur.rt != null) queue.offer(cur.rt);
+            }
+            L++;
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution0_8 tree = new Solution0_8();
+        tree.root = new Node(1);
+        tree.root.lt = new Node(2);
+        tree.root.rt = new Node(3);
+        tree.root.lt.lt = new Node(4);
+        tree.root.lt.rt = new Node(5);
+        tree.root.rt.lt = new Node(6);
+        tree.root.rt.rt = new Node(7);
+        tree.BFS(tree.root);
+    }
+}
