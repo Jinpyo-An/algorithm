477e72c4b0c89815a97e6a50f92edaae128ac4dc
Jinpyo An <dkswlsvy3312@gmail.com>
2026-03-05 13:35:54 +0900

    9. Tree 말단노드까지의 까장 짧은 경로(DFS)

---
+++ online-lecture/src/main/java/chapter07/Solution2_1.java
@@ -0,0 +1,21 @@
+package chapter07;
+
+import java.io.IOException;
+
+public class Solution2_1 {
+
+    public static int DFS(int L, Node root) {
+        if (root.lt == null && root.rt == null) return L;
+        else return Math.min(DFS(L + 1, root.lt), DFS(L + 1, root.rt));
+    }
+
+    public static void main(String[] args) throws IOException {
+        Node root = new Node(1);
+        root.lt = new Node(2);
+        root.rt = new Node(3);
+        root.lt.lt = new Node(4);
+        root.lt.rt = new Node(5);
+
+        System.out.println(DFS(0, root));
+    }
+}
