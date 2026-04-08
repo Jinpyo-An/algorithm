28586f589a221b17b0e57b2ba1149e56b85c6e10
Jinpyo An <dkswlsvy3312@gmail.com>
2026-04-08 11:06:01 +0900

    LEVEL3 - 삼각형

---
+++ question-bank/src/programers/level03/Solution1.java
@@ -0,0 +1,16 @@
+package programers.level03;
+
+public class Solution1 {
+
+    public int solution(int[][] triangle) {
+        int height = triangle.length;
+
+        for (int i = height - 2; i >= 0; i--) {
+            for (int j = 0; j < triangle[i].length; j++) {
+                triangle[i][j] += Math.max(triangle[i + 1][j], triangle[i + 1][j + 1]);
+            }
+        }
+
+        return triangle[0][0];
+    }
+}
